83d5b1d9510ca4ab9d68e9bb3646fdbff1f7fba6
wowshub <wowshub@gmail.com>
2024-07-06 22:56:06 +0800

    tkinter 更换图标 窗口大小 背景 最大最小 属性

--- 02Tkinter/01tkinterWin.py
+++ 02Tkinter/01tkinterWin.py
@@ -4,9 +4,20 @@
 from tkinter import *
 win = Tk()
 win.title("yangyangmir2")
-txt=Label(win,text="\n\nyangyang传奇2\n\n").pack()
-win.geometry("1024x768-0-0")
+txt=Label(win,text="\n\nyangyang传奇2\n\n",bg="yellow").pack()
+
+scrw=win.winfo_screenwidth()
+scrh=win.winfo_screenheight()
+x= (scrw-1024)/2
+y=(scrh-768)/2
+win.geometry('%dx%d+%d+%d' % (1024,768,x,y))
+# win.geometry("1024x768-0-0")
 win.configure(bg="yellow")
 win.maxsize(1024,1024)
+win.minsize(768,768)
+win.resizable(0,0)
+# win.state("zoomed")
+# win.iconify()
+win.iconbitmap('icon57.ico')
 # iconbitmap("")
 win.mainloop()
